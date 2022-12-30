4d5fc12264b9d5ac651b0e7a441c6710d820f127
Jefferson Lopes <jeffbreno@gmail.com>
2022-12-30 16:50:53 -0300

    Alura-Tracker 30122022_1650

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,13 +1,14 @@
 <template>
   <header>
     <h1>
-      <img src="../assets/logo.png" width="100" alt="LogoMarca">
+      <img src="../assets/logo.png" alt="LogoMarca">
     </h1>
   </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+
 export default defineComponent({
   name: 'BarraLateral'
 });
